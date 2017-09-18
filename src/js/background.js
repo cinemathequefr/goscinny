a847e949b171fd192d6b827133aad9e23db40a6f
nltesown <nlte@magic.fr>
2017-09-18 17:41:47 +0200

    Cleanup, etc.

--- src/js/background.js
+++ src/js/background.js
@@ -1,27 +1,28 @@
-
-
 var arc = d3.arc().innerRadius(50).outerRadius(2000);
+var bg;
+var rotate;
 
 function init (count) {
   count = count || 40;
 
-  var v = d3
-    .selectAll("svg")
+  bg = d3
+    .selectAll(".backgroundContainer")
+    .append("svg")
     .append("g")
     .append("g")
-    .attr("class", "rotating")
-    .selectAll("path").data(new Array(count)).enter().append("path").attr("d", function (i, j, k) {
-      var a = (2 * Math.PI) / count;
-      return arc({
-        startAngle: (j * a),
-        endAngle: (j * a) + (a / 2)
-      });
+    .classed("rotate", true);
+
+  bg.selectAll("path").data(new Array(count)).enter().append("path").attr("d", function (i, j, k) {
+    var a = (2 * Math.PI) / count;
+    return arc({
+      startAngle: (j * a),
+      endAngle: (j * a) + (a / 2)
     });
+  });
 
   resize();
+  rotate.start();
 }
-
-
 
 function resize () {
   d3.select("svg")
@@ -31,8 +32,14 @@ function resize () {
     .attr("transform", "translate(" + (window.innerWidth / 2) + "," + (window.innerHeight * 0.667) + ")");
 }
 
+rotate = {
+  start: () => { bg.classed("stop", false); },
+  stop: () => { bg.classed("stop", true); },
+  toggle: () => { bg.classed("stop", !bg.classed("stop")); }
+};
 
 export default {
   init: init,
-  resize: resize
+  resize: resize,
+  rotate: rotate
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,31 @@
+import route from "riot-route";
 import background from "./background.js";
 
-
-
-
-
-
 $(() => {
-  background.init();
+  $.getJSON("data/data.json").then(run);
 });
 
-$(window)
-.on("resize", () => {
+$(window).on("resize", () => {
   background.resize();
 });
 
-$("body").on("click", () => {
-  background.fast();
-});
+function run (data) {
+  background.init();
+  $("body").on("click", background.rotate.toggle);
 
+  // Routing
 
+  route("/", () => {
+  });
 
+  route("/*", function (code) {
+    var item = _(data).find({ "code": code });
+    if (item === undefined) {
+      route("/");
+    } else {
+      // DO SOMETHING
+    }
+  });
 
-
+  route.start(true);
+}
